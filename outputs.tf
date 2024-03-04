a01a58e09be3f4374b138fbda799a5d2b9c6e068
Kilian <kilianc0709@gmail.com>
2024-03-04 19:37:22 +0100

    fix: grammar in descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
 output "id" {
-  description = "The ID of the VPC"
+  description = "The ID of the VPC."
   value       = try(aws_vpc.main.id, null)
 }
 
 output "public_subnets" {
-  description = "List of IDs of the public subnets"
+  description = "List of IDs of the public subnets."
   value       = try(aws_subnet.public[*].id, null)
 }
 
 output "private_subnets" {
-  description = "List of IDs of the private subnets"
+  description = "List of IDs of the private subnets."
   value       = try(aws_subnet.private[*].id, null)
 }
 
 output "internet_gw" {
-  description = "The ID of the Internet Gateway"
+  description = "The ID of the Internet Gateway."
   value       = try(aws_internet_gateway.main.id, null)
 }
 
 output "nat_gws" {
-  description = "List of IDs of the NAT Gateways"
+  description = "List of IDs of the NAT Gateways."
   value       = try(aws_nat_gateway.main[*].id, null)
 }
