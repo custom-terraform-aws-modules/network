28fa5266f23973dc3a3e9d4ba84be06ff694f750
kfc-manager <kilianc0709@gmail.com>
2024-04-29 13:54:44 +0200

    enha: enabled private DNS namespaces

--- main.tf
+++ main.tf
@@ -3,7 +3,9 @@
 ################################
 
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr
+  cidr_block           = var.cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = var.tags
 }
